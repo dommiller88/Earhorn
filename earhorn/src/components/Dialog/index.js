c4cc5081e41d12e7148fb086334d1075c0aa6a1b
dommiller88 <mill2525@purdue.edu>
2019-10-28 20:07:30 -0400

    added dialog

---
+++ earhorn/src/components/Dialog/index.js
@@ -0,0 +1,59 @@
+import React, {Component, Fragment} from 'react'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+
+export default class Dialog extends Component{
+    state = {
+        open: false
+    }
+
+    handleToggle = () => {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+    render() {
+        const {open} = this.state
+
+    return <Fragment>
+        <IconButton aria-label="add" className="Add-button" onClick={this.handleToggle}>
+            <AddIcon />
+        </IconButton>
+
+        <Dialog open={open} onClose={this.handleToggle}  >
+            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Enter a song name to post
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button color="primary">
+                    Submit
+                </Button>
+            </DialogActions>
+      </Dialog>
+
+    </Fragment>
+    }
+ 
+}
+
+export default Dialog;
+    
--- earhorn/src/components/Header/index.js
+++ earhorn/src/components/Header/index.js
@@ -1,14 +1,27 @@
     // src/components/Header/index.js
-import React from "react";
+import React, { Component } from 'react';
 import "./header.css"
+import Dialog from "components/Dialog";
+import ReactDOM from 'react-dom';
 
-class Header extends React.Component{
+
+class Header extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            TextField: "hello"
+        }
+    }
     render(){
         return (
             <nav className="Nav">
                 <div className="Nav-menus">
                     <div className="Nav-brand">
                         <h1>Earhorn</h1>
+                        <div>
+                            <Dialog />
+                        </div>
                    </div>
                  </div>
             </nav>
